7070a1ec235055c557b23788320174ca3196c87a
mario arriola <riblarsv@gmail.com>
2023-12-14 03:22:27 +0100

    fist commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header(){
+    return (
+        <div>This is the Header</div>
+    )
+}
+
+export  function HeaderAnother(){
+    return (
+        <h3>this is another header</h3>
+    )
+}
---
+++ src/components/NewComponent.jsx
@@ -0,0 +1,8 @@
+export const  ComponentNew = (props) =>{
+    return(
+        <>
+        <h1 style={{color:"red"}}>Hello</h1>
+        <h2>{props.color}</h2>
+        </>
+    )
+}
